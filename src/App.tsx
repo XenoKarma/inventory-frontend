adad0777e1d038ad178f92e14bdc6a7b690b6299
XenoKarma <andikapurnama1803@gmail.com>
2026-05-02 00:16:04 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-purple-600">
+        Tailwind Ready 🚀
+      </h1>
+    </>
+  )
+}
+
+export default App
